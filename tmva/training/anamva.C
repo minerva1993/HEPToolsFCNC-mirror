e6d14e7514ed55bd9f92acfaf4e1a3569a11269f
Jiwon Park <jipark@cern.ch>
2017-05-18 15:34:11 +0900

    bjet DPhi and bjet DEta trainning

--- tmva/training/anamva.C
+++ tmva/training/anamva.C
@@ -7,29 +7,40 @@ void anamva(){
 
   TMVA::Factory *factory = new TMVA::Factory("TMVAClassification",outputFile,"V:!Silent:Color:Transformations=I:DrawProgressBar:AnalysisType=Classification"); 
 
-  Double_t signalWeight = 1.0;
+  Double_t signalWeight = 0.25;
   Double_t backgroundWeight= 1.0;
 
-  TFile* signal = new TFile("tmva_class_example.root");
-  TTree* sigTree = (TTree*)(signal->Get("TreeS"));
+  TFile* signalA = new TFile("tmva_AntiTop_Hut.root");
+  TTree* sigTreeA = (TTree*)(signalA->Get("tmva_AntiTop_Hut"));
+  TFile* signalB = new TFile("tmva_AntiTop_Hct.root");
+  TTree* sigTreeB = (TTree*)(signalB->Get("tmva_AntiTop_Hct"));
+  TFile* signalC = new TFile("tmva_Top_Hut.root");
+  TTree* sigTreeC = (TTree*)(signalC->Get("tmva_Top_Hut"));
+  TFile* signalD = new TFile("tmva_Top_Hct.root");
+  TTree* sigTreeD = (TTree*)(signalD->Get("tmva_Top_Hct"));
+
    
-  TFile* data = new TFile("tmva_class_example.root");
-  TTree* dataTree = (TTree*)(data->Get("TreeB"));
+  TFile* background = new TFile("tmva_ttbb.root");
+  TTree* backgroundTree = (TTree*)(background->Get("tmva_ttbb"));
 
-  factory->AddSignalTree(sigTree,signalWeight);
-  factory->AddBackgroundTree(dataTree,backgroundWeight);
+  factory->AddSignalTree(sigTreeA,signalWeight);
+  factory->AddSignalTree(sigTreeB,signalWeight);
+  factory->AddSignalTree(sigTreeC,signalWeight);
+  factory->AddSignalTree(sigTreeD,signalWeight);
+  factory->AddBackgroundTree(backgroundTree,backgroundWeight);
 
-  TCut mycuts="(var1 > -9.0|| var2 > -9.0)";
-  TCut mycutb="var3 > -9.0"; 
+  TCut mycuts="(BJMDPhi < 10 && BJMDEta < 10)";
+  TCut mycutb="(BJMDPhi < 10 && BJMDEta < 10)";
 
-  factory->AddVariable("var1",'F');
-  factory->AddVariable("var2",'F');
-  factory->AddVariable("var3",'F');  
+//  factory->AddVariable("NBJets_M",'F');
+  factory->AddVariable("BJMDPhi",'F');
+  factory->AddVariable("BJMDEta",'F');  
+
 
   factory->PrepareTrainingAndTestTree(mycuts,mycutb,"random"); 
 
   factory->BookMethod( TMVA::Types::kMLP, "MLP", "H:!V:NeuronType=tanh:VarTransform=N:NCycles=600:HiddenLayers=N+5:TestRate=5:!UseRegulator" );
-  factory->BookMethod( TMVA::Types::kBDT, "BDT", "!H:!V:NTrees=50:MinNodeSize=2.5%:MaxDepth=3:BoostType=AdaBoost:UseBaggedBoost:SeparationType=GiniIndex");
+  factory->BookMethod( TMVA::Types::kBDT, "BDT", "!H:!V:NTrees=500:MinNodeSize=2.5%:MaxDepth=3:BoostType=AdaBoost:UseBaggedBoost:SeparationType=GiniIndex");
 
   factory->TrainAllMethods();
   
