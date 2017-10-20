35f532ff23e4932aaafd1dd5a5651c7700e510b4
Jiwon Park <minerva1993@gmail.com>
2017-10-20 11:42:22 +0900

    new configs
    Former-commit-id: 0c7cf03c5b5129d541341b2cd8aa8712b0ab6ca9

---
+++ analysis_jw/tmva/v5/application_full5.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+import os, sys
+from ROOT import *
+from array import array
+from subprocess import call
+from os.path import isfile
+import numpy as np
+
+# Setup TMVA
+TMVA.Tools.Instance()
+TMVA.PyMethodBase.PyInitialize()
+reader = TMVA.Reader("Color:!Silent")
+
+ch = 'Hct11'
+#ch = 'Hut11'
+
+tuples = sys.argv[1]
+
+# Load data
+data = TFile.Open('/home/minerva1993/fcnc/analysis_jw/tmva/v5/mkNtuple_v5/'+tuples)
+data_tree = data.Get('tmva_tree')
+
+target = TFile('output_'+ch+'_'+tuples,'RECREATE')
+tree = TTree("tree","tree")
+
+branches = {}
+for branch in data_tree.GetListOfBranches():
+  branchName = branch.GetName()
+  if branchName not in ["lepDPhi", "bjetmDR", "bjetmDEta", "bjetmDPhi", "dibjetsMass", "bjetPt_dibjetsm", "cjetPt", "transverseMass", "jet1phi", "jet2phi", "jet3phi", "jet4phi", "KinLepWMass", "KinLepTopMass", "KinHadWMass", "KinHadTopMass", "FCNHKinLepWMass", "FCNHKinLepTopMass", "FCNHKinHMass", "FCNHKinHucTopMass", "M3LepWMass", "M3HadWMass", "M3HMass", "M3DR", "M3LepTopMass", "M3HucTopMass", "DRlepWphi", "DRjet0phi", "DRjet1phi", "DRjet2phi", "DRjet3phi", "DRjet12phi", "DRjet23phi", "DRjet31phi", "DRlepTphi", "DRhadTphi", "EventWeight"]:
+      branches[branchName] = array('f', [-999])
+      reader.AddVariable(branchName, branches[branchName])
+      data_tree.SetBranchAddress(branchName, branches[branchName])
+
+reader.BookMVA('PyKeras', TString('/home/minerva1993/fcnc/analysis_jw/tmva/v5/keras5_'+ch+'/weights/TMVAClassification_PyKeras.weights.xml'))
+reader.BookMVA('BDT', TString('/home/minerva1993/fcnc/analysis_jw/tmva/v5/keras5_'+ch+'/weights/TMVAClassification_BDT.weights.xml'))
+
+print "processing "+tuples
+nevt = data_tree.GetEntries()
+print "this sample contains "+str(nevt)+" events"
+
+score1 = np.zeros(1, dtype=float)
+score2 = np.zeros(1, dtype=float)
+event_weight = np.zeros(1, dtype=float)
+
+tree.Branch('KerasScore', score1, 'KerasScore/D')
+tree.Branch('BDTScore', score2, 'BDTScore/D')
+tree.Branch('Event_Weight', event_weight, 'Event_Weight/D')
+
+for i in xrange(nevt):
+  data_tree.GetEntry(i)
+  score1[0] = reader.EvaluateMVA('PyKeras')
+  score2[0] = reader.EvaluateMVA('BDT')
+
+  event_weight[0] = data_tree.EventWeight
+  #print data_tree.EventWeight
+
+  s = str(i)
+
+  if nevt > 100000:
+    if i%20000 == 0: print "process "+s
+  elif nevt > 10000 and nevt < 100000:
+    if i%10000 == 0: print "process "+s
+  elif nevt < 10000:
+    if i%1000 == 0: print "process "+s
+
+  tree.Fill()
+
+  if i == nevt:
+    del score1, score2, branches
+
+target.Write()
+target.Close()
--- analysis_jw/tmva/v5/score_mva/runAnalysis.py
+++ analysis_jw/tmva/v5/score_mva/runAnalysis.py
@@ -14,18 +14,16 @@ def runAna(dir, file, name):
 
 #p = TProof.Open("", "workers=8")
 
-ch = "Hct1"
-#ch = "Hut1"
+ch = "Hct11"
+#ch = "Hut11"
 version = "v5"
 inputdir = "/home/minerva1993/fcnc/analysis_jw/tmva/"+version+"/score_mva/"+ch
-"""
+
 runAna(inputdir, "output_"+ch+"_tmva_AntiTop_Hct.root",  ch+"_AntiTop_Hct")
 runAna(inputdir, "output_"+ch+"_tmva_AntiTop_Hut.root", ch+"_AntiTop_Hut")
 runAna(inputdir, "output_"+ch+"_tmva_Top_Hct.root", ch+"_Top_Hct")
 runAna(inputdir, "output_"+ch+"_tmva_Top_Hut.root", ch+"_Top_Hut")
-"""
 runAna(inputdir, "output_"+ch+"_tmva_SingleLepton_Run2016.root", ch+"_SingleLepton_Run2016")
-"""
 runAna(inputdir, "output_"+ch+"_tmva_ttbb.root", ch+"_ttbb")
 runAna(inputdir, "output_"+ch+"_tmva_ttbj.root", ch+"_ttbj")
 runAna(inputdir, "output_"+ch+"_tmva_ttcc.root", ch+"_ttcc")
@@ -35,8 +33,14 @@ runAna(inputdir, "output_"+ch+"_tmva_tbarchannel.root", ch+"_tbarchannel")
 runAna(inputdir, "output_"+ch+"_tmva_tbarWchannel.root", ch+"_tbarWchannel")
 runAna(inputdir, "output_"+ch+"_tmva_tchannel.root", ch+"_tchannel")
 runAna(inputdir, "output_"+ch+"_tmva_tWchannel.root", ch+"_tWchannel")
+"""
 runAna(inputdir, "output_"+ch+"_tmva_zjets10to50.root", ch+"_zjets10to50")
 runAna(inputdir, "output_"+ch+"_tmva_zjets.root", ch+"_zjets")
+runAna(inputdir, "output_"+ch+"_tmva_wjets.root", ch+"_wjets")
+runAna(inputdir, "output_"+ch+"_tmva_ww.root", ch+"_ww")
+runAna(inputdir, "output_"+ch+"_tmva_wz.root", ch+"_wz")
+runAna(inputdir, "output_"+ch+"_tmva_zz.root", ch+"_zz")
+
 runAna(inputdir, "output_"+ch+"_tmva_QCD_EGEnr_120to170.root", ch+"_QCD_EGEnr_120to170")
 runAna(inputdir, "output_"+ch+"_tmva_QCD_EGEnr_170to300.root", ch+"_QCD_EGEnr_170to300")
 runAna(inputdir, "output_"+ch+"_tmva_QCD_EGEnr_300toInf.root", ch+"_QCD_EGEnr_300toInf")
