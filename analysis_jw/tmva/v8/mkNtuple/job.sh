87616b5aa4a95fc3365980daeac8a241dd21a9e8
Jiwon Park <minerva1993@gmail.com>
2018-01-27 04:36:57 +0900

    more statistics, new limits
    Former-commit-id: 9b723a8b635a4c5bd03e7a86986561f81f9cada5 [formerly 4fd6b172f0682ca1e825901195a18dc95c07e4be]
    Former-commit-id: ddfd89b1b07091ba23f3f96e550144635e925003
    Former-commit-id: 5b3c2939d22c44f7938db16ddc3608fa08a09822
    Former-commit-id: 5eae6ea74132721b470f6e727d0537c810226136

--- analysis_jw/tmva/v8/mkNtuple/job.sh
+++ analysis_jw/tmva/v8/mkNtuple/job.sh
@@ -7,10 +7,10 @@ python runAnalysis.py $version $inputdir "TT_powheg_ttbj.root" "ttbj"&
 python runAnalysis.py $version $inputdir "TT_powheg_ttcc.root" "ttcc"&
 python runAnalysis.py $version $inputdir "TT_powheg_ttother.root" "ttother"&
 python runAnalysis.py $version $inputdir "DYJets.root" "zjets"&
-python runAnalysis.py $version $inputdir "WJets_part2.root" "wjets"&
+python runAnalysis.py $version $inputdir "WJets_v2.root" "wjetsV2"&
 python runAnalysis.py $version $inputdir "SingleTbar_t.root" "tbarchannel"&
 python runAnalysis.py $version $inputdir "SingleTop_t.root" "tchannel"&
-python runAnalysis.py $version $inputdir "DYJets_10to50.root" "zjets10to50"&
+python runAnalysis.py $version $inputdir "DYJets_10to50_v2.root" "zjets10to50V2"&
 python runAnalysis.py $version $inputdir "WW.root" "ww"&
 python runAnalysis.py $version $inputdir "WZ.root" "wz"&
 python runAnalysis.py $version $inputdir "ZZ.root" "zz"&
