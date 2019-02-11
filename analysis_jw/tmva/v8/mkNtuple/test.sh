f83dfd0b42d500807c3d512c2f372e4117e5c2e2
Jiwon Park <minerva1993@gmail.com>
2019-02-11 17:51:17 +0900

    arrange codes for 2018 analysis
    Former-commit-id: b9fcadf944a6dc02f234973f59a61c7d77519a6a [formerly 652a83f68bf5b5ac5586beee34cef38de0a0aa37]
    Former-commit-id: 43b69422f10ee0a24e8c7c39f83a2c120605ca36
    Former-commit-id: da8e14c9b89f1e9160fdf42d2e540abcd24cf413
    Former-commit-id: bd71079620392aea36babfdf7bd17bc04b868f26

--- analysis_jw/tmva/v8/mkNtuple/test.sh
+++
@@ -1,5 +0,0 @@
-version="v4"
-inputdir="/home/minerva1993/fcnc/ntuple_jw/"
-
-python runAnalysis.py $version $inputdir "WJets_v2.root" "wjetsV2"&
-python runAnalysis.py $version $inputdir "DYJets_10to50_v2.root" "zjets10to50V2"&
