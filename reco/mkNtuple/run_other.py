b3c2439c5df1f7869d30d7993e2b4cd7e0ff2fc6
Jiwon Park <minerva1993@gmail.com>
2019-04-08 10:44:14 +0900

    change path
    Former-commit-id: 26d57453764be8a025678901fa2fbc9c4fcea0f8

--- reco/mkNtuple/run_other.py
+++ reco/mkNtuple/run_other.py
@@ -20,12 +20,12 @@ syst = ["","jecup","jecdown","jerup","jerdown"]
 syst2 = ["TuneCP5up","TuneCP5down","hdampup","hdampdown"] #dedecative samples exist
 
 for syst_ext in syst + syst2:
-#  if not os.path.exists( '/data/users/minerva1993/work/' + str(int(era)+1) + '_fcnc_RunII' + era + '/reco/current_ver/root_' + reco_scheme + syst_ext ):
-  if not os.path.exists( era + "/root_" + reco_scheme + syst_ext ):
+  if not os.path.exists( '/data/users/minerva1993/work/' + str(int(era)+1) + '_fcnc_RunII' + era + '/reco/current_ver/root_' + reco_scheme + syst_ext ):
+#  if not os.path.exists( era + "/root_" + reco_scheme + syst_ext ):
     print "No folder"
     sys.exit()
-#  if not os.path.exists( '/data/users/minerva1993/work/' + str(int(era)+1) + '_fcnc_RunII' + era + '/reco/current_ver/hdf_' + reco_scheme + syst_ext ):
-  if not os.path.exists( era + "/hdf_" + reco_scheme + syst_ext ):
+  if not os.path.exists( '/data/users/minerva1993/work/' + str(int(era)+1) + '_fcnc_RunII' + era + '/reco/current_ver/hdf_' + reco_scheme + syst_ext ):
+#  if not os.path.exists( era + "/hdf_" + reco_scheme + syst_ext ):
     print "No folder"
     sys.exit()
 
@@ -45,23 +45,23 @@ def runAna(input_filename, output_filename):
         print "The file " + output_filename + ".root has zero entry"
       else: chain.Process("makeOtherTuple.C+", era + reco_scheme + syst_ext + '_' + output_filename)
 
-      if os.path.isfile(era + '/root_' + reco_scheme + syst_ext + '/deepReco_' + output_filename + '.root'):
-        f = TFile.Open(era + '/root_' + reco_scheme + syst_ext + '/deepReco_' + output_filename + '.root')
-        t = f.Get('test_tree')
-        if t.GetEntries() != 0:
-          a = tree2array(t)
-          df = pd.DataFrame(a)
-          io.save(era+ '/hdf_' + reco_scheme + syst_ext + '/deepReco_' + output_filename + '.h5', df)
-        else: os.remove(era + '/root_' + reco_scheme + syst_ext + '/deepReco_' + output_filename + '.root')
-
-#      if os.path.isfile('/data/users/minerva1993/work/' + str(int(era)+1) + '_fcnc_RunII' + era + '/reco/current_ver/root_' + reco_scheme + syst_ext + '/deepReco_' + output_filename + '.root'):
-#        f = TFile.Open('/data/users/minerva1993/work/' + str(int(era)+1) + '_fcnc_RunII' + era + '/reco/current_ver/root_' + reco_scheme + syst_ext + '/deepReco_' + output_filename + '.root')
+#      if os.path.isfile(era + '/root_' + reco_scheme + syst_ext + '/deepReco_' + output_filename + '.root'):
+#        f = TFile.Open(era + '/root_' + reco_scheme + syst_ext + '/deepReco_' + output_filename + '.root')
 #        t = f.Get('test_tree')
 #        if t.GetEntries() != 0:
 #          a = tree2array(t)
 #          df = pd.DataFrame(a)
-#          io.save('/data/users/minerva1993/work/' + str(int(era)+1) + '_fcnc_RunII' + era + '/reco/current_ver/hdf_' + reco_scheme + syst_ext + '/deepReco_' + output_filename + '.h5', df)
-#        else: os.remove('root_' + reco_scheme + syst_ext + '/deepReco_' + output_filename + '.root')
+#          io.save(era+ '/hdf_' + reco_scheme + syst_ext + '/deepReco_' + output_filename + '.h5', df)
+#        else: os.remove(era + '/root_' + reco_scheme + syst_ext + '/deepReco_' + output_filename + '.root')
+
+      if os.path.isfile('/data/users/minerva1993/work/' + str(int(era)+1) + '_fcnc_RunII' + era + '/reco/current_ver/root_' + reco_scheme + syst_ext + '/deepReco_' + output_filename + '.root'):
+        f = TFile.Open('/data/users/minerva1993/work/' + str(int(era)+1) + '_fcnc_RunII' + era + '/reco/current_ver/root_' + reco_scheme + syst_ext + '/deepReco_' + output_filename + '.root')
+        t = f.Get('test_tree')
+        if t.GetEntries() != 0:
+          a = tree2array(t)
+          df = pd.DataFrame(a)
+          io.save('/data/users/minerva1993/work/' + str(int(era)+1) + '_fcnc_RunII' + era + '/reco/current_ver/hdf_' + reco_scheme + syst_ext + '/deepReco_' + output_filename + '.h5', df)
+        else: os.remove('root_' + reco_scheme + syst_ext + '/deepReco_' + output_filename + '.root')
 
 runAna(input_filename, output_filename)
 
