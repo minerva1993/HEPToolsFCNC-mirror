8c4fe14d9df71430455c445c14b3cecf6e7747e5
Jiwon Park <minerva1993@gmail.com>
2019-08-05 20:44:16 +0900

    no more shitty syst. table. job
    Former-commit-id: f025ece71aa46b2d5c9cf86886d515223e5166bc [formerly 0a35839272c09884944512a837aaf1f4ade571fd]
    Former-commit-id: 242c47e6d12f700b92dd7b63ea1e867cb6be3516
    Former-commit-id: 476fb22c0f525bbabdcf4d703dffd179a21373eb
    Former-commit-id: bdd70b096088d54009ca4ed57d8b9c2f8c5f16d9

---
+++ finalMVA/histos/print_syst_table.py
@@ -0,0 +1,85 @@
+import os, shutil, re, sys
+from subprocess import call
+from collections import OrderedDict
+
+if len(sys.argv) < 4:
+  print("Specify year: 2017/2018, Ver, systematic type: all / source by source (a/s)")
+  sys.exit()
+era = sys.argv[1]
+ver = sys.argv[2]
+tmp_all = sys.argv[3]
+
+config_path = '../../plotIt/configs/'
+
+if tmp_all == "s": isAll = False
+else: isAll = True
+
+tmp_file_name = 'temp_' + era + '_' + ver + '.yml'
+string_to_add = 'systematics:\n'
+plot_to_add = "plots:\n  include: ['histos_unc.yml']\n\n"
+
+if os.path.exists(config_path + tmp_file_name):
+  os.remove(config_path + tmp_file_name)
+#shutil.copy2(config_path + 'config_' + era + '.yml', config_path + tmp_file_name)
+
+unc_cat = OrderedDict([
+('all', ['xsec', 'pu', 'muid', 'muiso', 'mutrg', 'elid', 'elreco', 'elzvtx', 'eltrg', 'jec', 'jer', 'scale', 'ps', 'hdamp', 'pdf', 'TuneCP5', 'lf', 'hf', 'lfstat1', 'lfstat2', 'hfstat1', 'hfstat2', 'cferr1', 'cferr2']),
+('pu', ['pu']),
+('xsec', ['xsec']),
+('lep', ['muid', 'muiso', 'mutrg', 'elid', 'elreco', 'elzvtx', 'eltrg']),
+('jec', ['jec']),
+('jer', ['jer']),
+('scale', ['scale']),
+('ps', ['ps']),
+('hdamp', ['hdamp']),
+('pdf', ['pdf']),
+('tune', ['TuneCP5']),
+('lf', ['lf']),
+('hf', ['hf']),
+('lfstat1', ['lfstat1']),
+('lfstat2', ['lfstat2']),
+('hfstat1', ['hfstat1']),
+('hfstat2', ['hfstat2']),
+('cferr1', ['cferr1']),
+('cferr2', ['cferr2']),
+('bAll', ['lf', 'hf', 'lfstat1', 'lfstat2', 'hfstat1', 'hfstat2', 'cferr1', 'cferr2']),
+])
+
+for key, value in unc_cat.items():
+  print "Print yield tables with source: " + key
+  #prep for systematics
+  with open(config_path + 'config_' + era + '.yml', 'r') as f:
+    with open(config_path + tmp_file_name, 'w') as f1:
+      syst_delete = False
+      plot_delete = False
+      for line in f:
+        if 'luminosity-error:' in line:
+          line = line[:line.find(':')+2]
+          line += "0.0\n"
+        if 'root:' in line:
+          line = line[:line.find(':')+2]
+          line += "'./" + era + "/Hct_merged_" + ver + "/merged/'\n"
+        if 'systematics' in line: syst_delete = True
+        if 'plots:' in line:
+          syst_delete = False
+          plot_delete = True
+        if 'legend' in line: plot_delete = False
+        if syst_delete == True: continue
+        if plot_delete == True: continue
+        f1.write(line)
+
+  with open(config_path + "syst_all.yml") as f:
+    lines = f.readlines()
+    with open(config_path + tmp_file_name, "a") as f1:
+      f1.writelines(plot_to_add)
+      for line in lines:
+        if any(x in line for x in value): string_to_add += str(line)
+      f1.writelines(string_to_add)
+  syst_postfix = key
+  call(['../../plotIt/plotIt', '-o ' + './' + era + '/Hct_merged_' + ver + '/' ,config_path + tmp_file_name, '-y'], shell=False)
+  with open('./' + era + '/Hct_merged_' + ver + '/yields.tex', 'r') as f:
+    with open('./' + era + '/Hct_merged_' + ver + '/yields_' + syst_postfix + '.tex', 'w+') as f1:
+      for line in f:
+        if any(x in line[0] for x in ['0','1','2','3']): line = line[1:]
+        f1.write(line)
+  string_to_add = 'systematics:\n'
