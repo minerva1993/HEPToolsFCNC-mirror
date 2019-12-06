5048fc286c05c2cdd6c97b4410e302a5007392b0
Jiwon Park <minerva1993@gmail.com>
2019-12-06 19:24:15 +0900

    test
    Former-commit-id: bf1683732a4349b8b673dba937019e1998fa22ac

--- finalMVA/mkNtuple/dir_manage.py
+++ finalMVA/mkNtuple/dir_manage.py
@@ -1,18 +1,23 @@
 #!/usr/bin/python
 import os, sys
 
+#base_path17 = '2017'
+#base_path18 = '2018'
+base_path17 = '/data1/users/minerva1993/work/fcnc_RunII2017/finalMVA/current_version'
+base_path18 = '/data1/users/minerva1993/work/fcnc_RunII2018/finalMVA/current_version'
+
 ch = ['', 'jecup', 'jecdown', 'jerup', 'jerdown',
       'hdampup', 'hdampdown', 'TuneCP5up', 'TuneCP5down',]
 
 for chs in ch:
-  if not os.path.exists( "2017/root_" + chs ):
-    os.makedirs( "2017/root_" + chs )
-  if not os.path.exists( "2017/hdf_" + chs ):
-    os.makedirs( "2017/hdf_" + chs )
-  if not os.path.exists( "2018/root_" + chs ):
-    os.makedirs( "2018/root_" + chs )
-  if not os.path.exists( "2018/hdf_" + chs ):
-    os.makedirs( "2018/hdf_" + chs )
+  if not os.path.exists( base_path17 + "/root_" + chs ):
+    os.makedirs( base_path17 + "/root_" + chs )
+  if not os.path.exists( base_path17 + "/hdf_" + chs ):
+    os.makedirs( base_path17 + "/hdf_" + chs )
+  if not os.path.exists( base_path18 + "/root_" + chs ):
+    os.makedirs( base_path18 + "/root_" + chs )
+  if not os.path.exists( base_path18 + "/hdf_" + chs ):
+    os.makedirs( base_path18 + "/hdf_" + chs )
 
 
 def nfile(dirname):
@@ -23,10 +28,10 @@ def nfile(dirname):
   print dirname + ": " + str(len(tmp)).rjust(30-len(dirname))
 
 for chs in ch:
-  nfile("2017/root_" + chs)
+  nfile(base_path17 + "/root_" + chs)
 for chs in ch:
-  nfile("2017/hdf_" + chs)
+  nfile(base_path17 + "/hdf_" + chs)
 for chs in ch:
-  nfile("2018/root_" + chs)
+  nfile(base_path18 + "/root_" + chs)
 for chs in ch:
-  nfile("2018/hdf_" + chs)
+  nfile(base_path18 + "/hdf_" + chs)
