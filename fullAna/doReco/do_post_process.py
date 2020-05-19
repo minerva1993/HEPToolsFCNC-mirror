c2618dea292fe599e531bcef47c2087998a174ee
Jiwon Park <minerva1993@gmail.com>
2020-05-19 17:38:25 +0900

    update post proc for all histos
    Former-commit-id: c693f41ec9957b53bc2f03508ec2d0d574e6a474

--- fullAna/doReco/do_post_process.py
+++ fullAna/doReco/do_post_process.py
@@ -2,7 +2,6 @@ from ROOT import *
 import ROOT
 import os
 
-#base_path = "/home/minerva1993/HEPToolsFCNC/analysis_2017/fullAna/doReco/"
 base_path = "./"
 if not os.path.exists( base_path + "post_process" ):
   os.makedirs( base_path + "post_process" )
@@ -13,7 +12,12 @@ def write_envelope(syst, nhists, new_sumW):
     var_list = []
     for x in range(0,nhists):
       h = f.Get(histos + "__" + syst + str(x))
-      h.Scale(EventInfo.GetBinContent(2) / new_sumW.GetBinContent(x+1))
+      if any(x in syst for x in ['scale', 'ps']):
+        pass
+      elif 'pdf' in syst:
+        if x == 0: continue
+        h.Scale(EventInfo.GetBinContent(2) / new_sumW.GetBinContent(1))
+      else: h.Scale(EventInfo.GetBinContent(2) / new_sumW.GetBinContent(x+1))
       var_list.append(h)
 
     nominal = f.Get(histos)
@@ -37,8 +41,8 @@ def write_envelope(syst, nhists, new_sumW):
       up.SetBinContent(i, maximum)
       down.SetBinContent(i, minimum)
 
-    up = bSFNorm(up)
-    down = bSFNorm(down)
+    up = bSFNorm(up, bSFInfo)
+    down = bSFNorm(down, bSFInfo)
     up.SetName(histos + "__" + syst + "up")
     down.SetName(histos + "__" + syst + "down")
     up.Write()
@@ -68,13 +72,71 @@ def rescale(binNum, new_sumW): # rescale up/dn histos
   elif mode == 1:
     if syst_name in files:
       h = f.Get(histos)
-      h.Scale(nom_EventInfo.GetBinContent(2) / EventInfo.GetBinContent(2))
+      if not any(i in h.GetName() for i in ['Info', 'Weight']):
+        h.Scale(nom_EventInfo.GetBinContent(2) / EventInfo.GetBinContent(2))
+
+        if any(low_stat in syst_name for low_stat in ['Tune', 'hdamp']):
+          bSFInfo_nom = fill_bSFInfo(nom_f)
+          h_nom = nom_f.Get(histos)
+          h_nom = bSFNorm(h_nom, bSFInfo_nom)
+
+          if 'down' in files:
+            f_opp = TFile.Open( os.path.join(pre_path, files.replace('down','up')), "READ")
+          elif 'up' in files:
+            f_opp = TFile.Open( os.path.join(pre_path, files.replace('up','down')), "READ")
+
+          opp_EventInfo = f_opp.Get('EventInfo')
+          bSFInfo_opp = fill_bSFInfo(f_opp)
+          h_opp = f_opp.Get(histos)
+          h_opp = bSFNorm(h_opp, bSFInfo_opp)
+          h_opp.Scale(nom_EventInfo.GetBinContent(2) / opp_EventInfo.GetBinContent(2))
+
+          for xbin in xrange(h.GetNbinsX()):
+            if h_nom.GetBinContent(xbin+1) == 0: h.SetBinContent(xbin+1, 0.)
+            else:
+              ratio = h.GetBinContent(xbin+1) / h_nom.GetBinContent(xbin+1)
+              ratio_opp = 1.
+              if h_opp.GetBinContent(xbin+1) > 0: ratio_opp = h.GetBinContent(xbin+1) / h_opp.GetBinContent(xbin+1)
+              diff = abs(h_nom.GetBinContent(xbin+1)-h.GetBinContent(xbin+1)) + abs(h_nom.GetBinContent(xbin+1)-h_opp.GetBinContent(xbin+1))
+              if ratio_opp > 1.:
+                h.SetBinContent(xbin+1, h_nom.GetBinContent(xbin+1) + diff/2.)
+                if ratio > 1.2: h.SetBinContent(xbin+1, 1.2 * h_nom.GetBinContent(xbin+1))
+              else:
+                h.SetBinContent(xbin+1, h_nom.GetBinContent(xbin+1) - diff/2.)
+                if ratio < 0.8: h.SetBinContent(xbin+1, 0.8 * h_nom.GetBinContent(xbin+1))
+
+#          for xbin in xrange(h.GetNbinsX()):
+#            if h_nom.GetBinContent(xbin+1) == 0: h.SetBinContent(xbin+1, 0.)
+#            else:
+#              ratio = h.GetBinContent(xbin+1) / h_nom.GetBinContent(xbin+1)
+#              if ratio > 1.2: h.SetBinContent(xbin+1, 1.2 * h_nom.GetBinContent(xbin+1))
+#              elif ratio < 0.8: h.SetBinContent(xbin+1, 0.8 * h_nom.GetBinContent(xbin+1))
+
       f_new.cd()
       h.Write()
 
 
-def bSFNorm(htmp):
-  if any(bSFInfo):
+def fill_bSFInfo(f_in):
+
+  bSFInfo_tmp = {}
+  bSFInfo_tmp['Ch0_J0'] = f_in.Get("bSFInfo_Ch0_J0")
+  bSFInfo_tmp['Ch0_J1'] = f_in.Get("bSFInfo_Ch0_J1")
+  bSFInfo_tmp['Ch0_J2'] = f_in.Get("bSFInfo_Ch0_J2")
+  bSFInfo_tmp['Ch1_J0'] = f_in.Get("bSFInfo_Ch1_J0")
+  bSFInfo_tmp['Ch1_J1'] = f_in.Get("bSFInfo_Ch1_J1")
+  bSFInfo_tmp['Ch1_J2'] = f_in.Get("bSFInfo_Ch1_J2")
+  bSFInfo_tmp['Ch2_J0'] = f_in.Get("bSFInfo_Ch2_J0")
+  bSFInfo_tmp['Ch2_J1'] = f_in.Get("bSFInfo_Ch2_J1")
+  bSFInfo_tmp['Ch2_J2'] = f_in.Get("bSFInfo_Ch2_J2")
+  if bSFInfo_tmp['Ch2_J0'].Integral() == 0 and bSFInfo_tmp['Ch2_J2'].Integral() == 0:
+    bSFInfo_tmp.clear() #If sum is 0, remove items: for data, J0 = 0 in MVA
+
+  return bSFInfo_tmp
+
+
+def bSFNorm(htmp, infos):
+
+  if any(infos):
     hname = htmp.GetName()
     keystr = ''
     if   'Ch0' in hname: keystr += 'Ch0_'
@@ -85,14 +147,14 @@ def bSFNorm(htmp):
       #case1: MVA -> j3b2, j3b3, j4b2, j4b3, j4b4
       if   'j3' in hname: keystr += 'J1'
       elif 'j4' in hname: keystr += 'J2'
-      infotmp = bSFInfo[keystr].Clone()
+      infotmp = infos[keystr].Clone()
     else:
       #case2: fullAna -> S1,2,3,5,6,7,8
       if   'S0'in hname: keystr += 'J0'
       elif any(i in hname for i in ['S1','S2','S3','S4']): keystr += 'J1'
       elif any(i in hname for i in ['S5','S6','S7','S8']): keystr += 'J2'
-      infotmp = bSFInfo[keystr].Clone()
-      if 'S4' in hname: infotmp.Add(bSFInfo[keystr.replace('J1','J2')], 1.0)
+      infotmp = infos[keystr].Clone()
+      if 'S4' in hname: infotmp.Add(infos[keystr.replace('J1','J2')], 1.0)
 
     binnum = 2 #nominal = 1
     if any(i in hname for i in ['__lf', '__hf', '__cferr']):
@@ -125,23 +187,13 @@ for files in file_list:
   EventInfo = f.Get("EventInfo")
   ScaleWeights = f.Get("ScaleWeights")
   PSWeights = f.Get("PSWeights")
-#  PDFWeights = f.Get("PDFWeights")
+  #PDFWeights = f.Get("PDFWeights")
   nScaleWeight = ScaleWeights.Integral()
   nPSWeight = PSWeights.Integral()
+  #nPDFWeight = PDFWeights.Integral()
 
   #bSF - 6 histos
-  bSFInfo = {}
-  bSFInfo['Ch0_J0'] = f.Get("bSFInfo_Ch0_J0")
-  bSFInfo['Ch0_J1'] = f.Get("bSFInfo_Ch0_J1")
-  bSFInfo['Ch0_J2'] = f.Get("bSFInfo_Ch0_J2")
-  bSFInfo['Ch1_J0'] = f.Get("bSFInfo_Ch1_J0")
-  bSFInfo['Ch1_J1'] = f.Get("bSFInfo_Ch1_J1")
-  bSFInfo['Ch1_J2'] = f.Get("bSFInfo_Ch1_J2")
-  bSFInfo['Ch2_J0'] = f.Get("bSFInfo_Ch2_J0")
-  bSFInfo['Ch2_J1'] = f.Get("bSFInfo_Ch2_J1")
-  bSFInfo['Ch2_J2'] = f.Get("bSFInfo_Ch2_J2")
-  if bSFInfo['Ch2_J0'].Integral() == 0 and bSFInfo['Ch2_J2'].Integral() == 0:
-    bSFInfo.clear() #If sum is 0, remove items: for data, J0 = 0 in MVA
+  bSFInfo = fill_bSFInfo(f)
 
   #Prepare nominal file for rescaling
   syst_name = ""
@@ -165,9 +217,10 @@ for files in file_list:
     if "__" not in histos: nominal_list.append(histos)
     if "scale" in histos: continue
     if "ps" in histos: continue
+    #if "pdf" in histos: continue
     h = f.Get(histos)
     if not any(i in h.GetName() for i in ['Info', 'Weight']):
-      h = bSFNorm(h)
+      h = bSFNorm(h, bSFInfo)
     else: pass
     h.Write()
 
@@ -176,7 +229,7 @@ for files in file_list:
 
     if nScaleWeight > 0: write_envelope("scale", 6, ScaleWeights)
     if nPSWeight > 0: write_envelope("ps", 4, PSWeights)
-    #write_envelope("pdf", histos, 103, PDFWeights)
+    #if nPDFWeight > 0: write_envelope("pdf", 103, PDFWeights)
     if run_on_syst: rescale([], nom_EventInfo)
 
   f_new.Write()
