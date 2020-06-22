3d3faaa2aeea00164f21c422d6bd8ac8db660375
Jiwon Park <minerva1993@gmail.com>
2020-06-22 10:59:51 +0900

    update scripts
    Former-commit-id: 26c14e1e329cf9392cdd38e154eaf7d9c271c55b

--- reco/mkNtuple/compile.sh
+++ reco/mkNtuple/compile.sh
@@ -1,3 +1,4 @@
-head -1 ./../../commonTools/file_2017_top.txt | xargs -i -n2 python run_top.py STFCNC 2017
-head -1 ./../../commonTools/file_2017_syst.txt | xargs -i -n2 python run_top.py STFCNC 2017
-head -1 ./../../commonTools/file_2017_other.txt | xargs -i -n2 python run_other.py STFCNC 2017
+YEAR=2017
+head -1 ./../../commonTools/file_${YEAR}_top.txt | xargs -i -n2 python run_top.py STFCNC ${YEAR}
+#head -1 ./../../commonTools/file_${YEAR}_syst.txt | xargs -i -n2 python run_top.py STFCNC ${YEAR}
+#head -1 ./../../commonTools/file_${YEAR}_other.txt | xargs -i -n2 python run_other.py STFCNC ${YEAR}
